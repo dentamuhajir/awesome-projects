99207a426292a1b439eb7b56e3e3bb3eeca9a281
Denta Muhajir <dentamuhajir@gmail.com>
2024-07-27 11:11:43 +0700

    Using Http to make Hello World in web application

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "awesomeProject/fundamental"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 
@@ -10,7 +13,16 @@ func main() {
 	//fundamental.Maps()
 	//fundamental.Slicing()
 	//fundamental.DecisionStructure()
-	fundamental.LoopAndRangingOverData()
-	fundamental.LoopAndRangingOverData()
+	//fundamental.LoopAndRangingOverData()
+	//fundamental.LoopAndRangingOverData()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		n, err := fmt.Fprintf(w, "Hello World")
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
+	})
+
+	_ = http.ListenAndServe(":8080", nil)
 
 }
